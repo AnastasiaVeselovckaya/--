8bef66ed661da8d2cbeff1829a8fc4fdc6c3c56c
user <user@WIN-SS6N9OIOE40>
2022-12-18 12:49:09 +0300

    Добавьте файлы проекта.

---
+++ Задание.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = File.ReadAllText(@"C:\Users\user\Desktop\Первый.txt");
+        Console.WriteLine(text);
+    }
+}
